e86f0018c6f2bebe1aab4c9495957f2a119a8766
Matheus Henrique <hmatheus870@gmail.com>
2021-11-20 15:10:25 -0300

    Versão inicial

---
+++ src/main/java/br/com/mhenrique/spring/boot/estudo/SpringBootEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mhenrique.spring.boot.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mhenrique/spring/boot/estudo/SpringBootEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mhenrique.spring.boot.estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
